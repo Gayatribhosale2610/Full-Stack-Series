015191e50312b85cbdfc9c0dca03a34313d0386a
Gayatri <gayatrivb26@gmail.com>
2024-02-05 20:05:49 +0530

    Type conversion

--- JS/test.js
+++ JS/test.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+//console.log("Hello")
+
+"use strict"; //treat all js code as a newer version
+
+
+// -------------------- conversion --------------------------
+
+let score = true
+console.log(typeof score);
+console.log(typeof (score));
+
+//convert string to number
+let strToNum = Number(score)
+console.log(typeof strToNum);
+console.log(strToNum);
+
+// to Number()
+// "33" => 33
+// "33da3"=> NaN
+// Null => 0
+// true => 1
+
+let a = 0
+let b = Boolean(a)
+console.log(b)
+
+// to Boolean()
+// 1 => true
+// 0 => false
+// "" => false
+// "abc" => true
+
+// to String()
