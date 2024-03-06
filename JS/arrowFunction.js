86a2de5c4470e095187f7c475a1e4f5527a9c528
Gayatri <gayatrivb26@gmail.com>
2024-03-06 22:50:17 +0530

    Arrow functions

---
+++ JS/arrowFunction.js
@@ -0,0 +1,14 @@
+// arrow function
+
+() => { }
+
+// ***************** Explicit return
+//const addFunc = (num1, num2) => {
+//    return num1 + num2;
+//}
+//console.log(addFunc(3, 4))
+
+// ***************** Implicit return 
+//const addFunc = (num1, num2) => num1 + num2;
+const addFunc = (num1, num2) => (num1 + num2);
+console.log(addFunc(3, 4))
